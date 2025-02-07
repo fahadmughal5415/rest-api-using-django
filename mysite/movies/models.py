84902db0c430a5bda62f748330f9a48ce1bd88a1
Programming Point <fahadmughal5415@gmail.com>
2025-02-07 16:10:05 +0500

    Change the default category name

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -14,4 +14,4 @@ class MoviesData(models.Model):
     name = models.CharField(max_length=200)
     duration = models.FloatField()
     rating = models.FloatField()
-    category = models.CharField(max_length=200, default='movies-category')
+    category = models.CharField(max_length=200, default='movies')
