2c924a30282d2c2e4983715a720ce449d7a1294e
Programming Point <fahadmughal5415@gmail.com>
2025-02-07 16:28:03 +0500

    Refactor code

--- mysite/movies/serializers.py
+++ mysite/movies/serializers.py
@@ -4,4 +4,4 @@ from .models import MoviesData
 class MoviesSerializer(serializers.ModelSerializer):
     class Meta:
         model = MoviesData
-        fields = ('id', 'name', 'duration', 'rating', 'category')
+        fields = ['id', 'name', 'duration', 'rating', 'category']
