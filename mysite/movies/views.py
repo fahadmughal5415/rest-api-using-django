ee26ae0b725793430d4fe6254a9265ef682f1104
Programming Point <fahadmughal5415@gmail.com>
2025-02-07 15:39:11 +0500

    Create Class based views and add router in urls

--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MoviesSerializer
+from .models import MoviesData
 
 # Create your views here.
+
+class MoviesViewSet(viewsets.ModelViewSet):
+    queryset = MoviesData.objects.all()
+    serializer_class = MoviesSerializer
